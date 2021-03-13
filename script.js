7a5b83efde83c5199aeaef51aff78b92776706e0
Piotr <piotrwydmuch95@gmail.com>
2021-03-13 16:42:37 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const state = window.state;
+window.state = {
+    "cars": {
+        0: {
+            'name': 'Polonez',
+            'year': '1990',
+            'engine': '2.0',
+        },
+        1: {
+            'name': 'Volvo',
+            'year': '1998',
+            'engine': '1.8',
+        },
+        2: {
+            'name': 'Fiat',
+            'year': '2000',
+            'engine': '1.2',
+        },
+
+    },
+}
+
+
+
+
